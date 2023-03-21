e6a5a7505068256d86a2e2bd4c86f8c7175bf75d
Brian Steven <sbrianprog@gmail.com>
2023-03-21 17:31:41 -0500

    update shipment

--- Deliverable3/Deliverable3/src/main/java/com/example/Deliverable3/Controller/ShipmentController/ShipmentController.java
+++ Deliverable3/Deliverable3/src/main/java/com/example/Deliverable3/Controller/ShipmentController/ShipmentController.java
@@ -1,13 +1,13 @@
 package com.example.Deliverable3.Controller.ShipmentController;
 
+import com.example.Deliverable3.Modules.Customer;
 import com.example.Deliverable3.Modules.Shipment;
 import com.example.Deliverable3.Service.ServiceShipment.ShipmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -19,5 +19,11 @@ public class ShipmentController {
     public ResponseEntity<Shipment> shipmentCreation(@RequestBody Shipment shipmentCreated){
         ResponseEntity<Shipment> response = shipmentService.processCreated(shipmentCreated);
         return response;
+    }
+
+    @GetMapping(params = {"stateShipment", "idCustomer"})
+    public List<Shipment> filtered(@RequestParam String stateShipment, @RequestParam String idCustomer) {
+        //capa logica
+        return shipmentService.filtered(stateShipment, idCustomer);
     }
 }
